6d4fca5c18176919d9f46d0f34eb79bd82626742
FivePixels <o5pxels@gmail.com>
2020-02-04 08:29:50 -0600

    Remove additional prelab file

--- lab2.1/prelab-2.1.c
+++ lab2.1/prelab-2.1.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 int main(void) {
 	for (int i=0; i < 34; i++) {
 		if (i > 0 && i % 3 == 0) {
--- lab2/prelab-2.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-	int x, y;
-	scanf("Enter 2 integers separated by spaces:", %d %d);
-}
-
